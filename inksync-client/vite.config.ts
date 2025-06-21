b95b678dd344bcc43cd329d737f53cce4e5a66be
Saravanaa Velpari <v.saravanaa@nagarro.com>
2025-06-21 14:31:16 +0530

    added react setup

---
+++ inksync-client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // or a specific IP address like '192.168.1.100'
+    port: 3333, // your desired port number
+  },
+})
